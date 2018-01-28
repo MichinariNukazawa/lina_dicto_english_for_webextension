4d2a4825cdbb4fc29300e577121eeadae10decef
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2018-01-28 09:43:55 +0900

    hover_property.js

---
+++ overwrite/js/hover_property.js
@@ -0,0 +1,19 @@
+'use strict';
+
+class HoverProperty{
+	static getRootBorderMatchedColor()
+	{
+		return 'rgba(0, 0, 170, 1)';
+	}
+
+	static getRootBorderCandidateColor()
+	{
+		return 'rgba(110, 190, 255, 1)';
+	}
+
+	static getRootBackgroundColor()
+	{
+		return 'rgba(200, 240, 255, 1)';
+	}
+}
+
